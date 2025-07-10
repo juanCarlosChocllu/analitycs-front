2e3dde14a3a634d0ff9bffbd6148051930cd1916
Jesus Campos <j.campos@opti-system.com>
2025-07-10 18:06:26 -0400

    añadir menus

--- src/features/app/Layout/Layout.tsx
+++ src/features/app/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { AlignJustify } from "lucide-react";
 import { MenuLateral } from "../components/Menu/MenuLateral";
 import { useState } from "react";
+import Sibar from "../components/Sibar";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const [open, setOpen] = useState(false);
@@ -9,6 +10,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   };
   return (
     <div>
+        <Sibar />
       <button
         onClick={toggleMenuOpen}
         className="bg-[#374152] rounded-full m-4 fixed top-0 left-0 p-4 w-12 h-12 flex items-center justify-center"
