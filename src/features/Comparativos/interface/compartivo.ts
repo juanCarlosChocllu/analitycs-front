e00b693f6808f532887251b165bf2adecdf07028
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-08-18 15:20:39 -0400

    cambios

--- src/features/Comparativos/interface/compartivo.ts
+++ src/features/Comparativos/interface/compartivo.ts
@@ -1,21 +1,19 @@
-
 export interface VentaItem {
   producto: string;
   cantidad: number;
   importe: number;
 }
 
-
 export interface dataI {
-  dataActual:ComparativoData
-  dataAnterior:ComparativoData
+  dataActual: ComparativoData;
+  dataAnterior: ComparativoData;
 }
 
-export  interface dataComparativo {
-      cantidadAnterior:number,
-                        ventaAnterior : number,
-                        cantidadActual: number,
-                        ventaActual:number
+export interface dataComparativo {
+  cantidadAnterior: number;
+  ventaAnterior: number;
+  cantidadActual: number;
+  ventaActual: number;
 }
 
 export interface VentaSucursalProducto {
@@ -39,41 +37,42 @@ export interface VentaSucursalResumenData {
 export interface VentaSucursal {
   cantidadSucursal: number;
   data: VentaSucursalResumenData;
- ventaSucursal:ventaSucursalI[]
+  ventaSucursal: ventaSucursalI[];
 }
 
-export interface ventaSucursalI{
-    sucursal:string
-    data:[{
-         producto:string
-    cantidad:number,
-    montoTotal:number
-    }]
+export interface ventaSucursalI {
+  sucursal: string;
+  data: [
+    {
+      producto: string;
+      cantidad: number;
+      montoTotal: number;
+    }
+  ];
 }
 
 export interface ComparativoData {
   cantidadSucursal: number;
-  fechaInicio: string;   
-  fechaFin: string;      
+  fechaInicio: string;
+  fechaFin: string;
   total: number;
   cantidad: number;
   ticketPromedio: number;
   venta: VentaItem[];
   ventaSucursal: VentaSucursal;
-  dataDiaria:DataDiaria[]
+  dataDiaria: DataDiaria[];
 }
 export interface PropsVentaI {
-    ventaActual:VentaItem[]
-    ventaAnterior:VentaItem[]
-    
+  ventaActual: VentaItem[];
+  ventaAnterior: VentaItem[];
 }
 
 export interface FilterComparativo {
-  sucursal:    string[];
+  sucursal: string[];
   fechaInicio: string;
-  fechaFin:    string;
-  flagVenta:   string;
-  tipoVenta:   string[];
+  fechaFin: string;
+  flagVenta: string;
+  tipoVenta: string[];
 }
 
 export interface DataDiaria {
@@ -82,7 +81,7 @@ export interface DataDiaria {
   ticket: number;
   producto: string;
   ticketPromedio: number;
-  precioPromedio:number;
+  precioPromedio: number;
   fecha: string;
 }
 
