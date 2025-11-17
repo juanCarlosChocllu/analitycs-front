72b5298e718127dc9839f696ed2db3d562f519f5
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-11-17 15:00:54 -0400

    cambios

--- src/features/cotizacion/components/CotizacionPorScursal.tsx
+++ src/features/cotizacion/components/CotizacionPorScursal.tsx
@@ -19,29 +19,39 @@ export const CotizacionPorScursal = ({
   const resultado = agruparCotizacionPorScursal(cotizacion);
 
   return (
-    <Box sx={{ padding: 3, backgroundColor: "grey.100", borderRadius: 3 }}>
-      <Typography variant="h5" gutterBottom sx={{ fontWeight: "bold" }}>
-        Cotizaciones por Sucursal
-      </Typography>
+   <Box sx={{ padding: 3, backgroundColor: "grey.100", borderRadius: 3 }}>
+  <Typography variant="h5" gutterBottom sx={{ fontWeight: "bold" }}>
+    Cotizaciones por Sucursal
+  </Typography>
 
-      <Box sx={{ width: "100%", height: 300 }}>
-        <ResponsiveContainer>
-          <BarChart
-            data={resultado}
-            margin={{ top: 20, right: 20, left: 0, bottom: 5 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="sucursal" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="cotizaciones" fill="#1976d2" barSize={40}>
-           
-              <LabelList dataKey="cotizaciones" position="top" />
-            </Bar>
-          </BarChart>
-        </ResponsiveContainer>
-      </Box>
-    </Box>
+  <Box sx={{ width: "100%", height: 300 }}>
+    <ResponsiveContainer>
+      <BarChart
+        data={resultado}
+        margin={{ top: 20, right: 20, left: 0, bottom: 20 }}  
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+
+       
+        <XAxis
+          dataKey="sucursal"
+          angle={-20}          
+          textAnchor="end"     
+          interval={0}         
+          height={80}         
+        />
+
+        <YAxis />
+        <Tooltip />
+
+        <Bar dataKey="cotizaciones" fill="#1976d2" barSize={40}>
+          <LabelList dataKey="cotizaciones" position="top" />
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  </Box>
+</Box>
+
   );
 };
 
